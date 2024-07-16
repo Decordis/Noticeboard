64c75b082a172d289cc78aeae9d7ba57639347ca
Decrodis <decordis145@gmail.com>
2024-07-16 16:49:28 +0300

    6 commit

--- newspaper/simpleapp/signals.py
+++ newspaper/simpleapp/signals.py
@@ -1,3 +1,4 @@
+# !!!!!!Работа с классическим сигналом!!!!!
 # from django.db.models.signals import m2m_changed
 # from django.dispatch import receiver
 #
@@ -21,4 +22,16 @@
 #
 #
 
+# Работа через celery+redis!!!!!
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+
+from .models import PostCategory
+from .tasks import send_email_task
+
+@receiver(m2m_changed, sender=PostCategory)
+def notify_about_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        send_email_task.delay(instance.pk)
 
