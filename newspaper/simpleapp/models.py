d6fa2723f94ae63ad1c4629ba4bed6a962fb6832
Decrodis <decordis145@gmail.com>
2024-08-23 17:49:01 +0300

    6 commit

--- newspaper/simpleapp/models.py
+++ newspaper/simpleapp/models.py
@@ -5,7 +5,6 @@ from django.db.models import Sum
 from django.db.models.functions import Coalesce
 
 
-
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
@@ -17,6 +16,7 @@ class Author(models.Model):
 
         self.rating = post_rating * 3 + comments_rating + posts_comments_rating
         self.save()
+
 
 class Post(models.Model):
     news = 'NW'
@@ -46,13 +46,11 @@ class Post(models.Model):
         self.rating -= 1
         self.save()
 
-
     def __str__(self):
         return f'{self.title.title()}: {self.description[:20]}'
 
     def get_absolute_url(self):
         return reverse('news_detail', args=[str(self.id)])
-
 
 
 class Category(models.Model):
@@ -66,6 +64,7 @@ class Category(models.Model):
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
